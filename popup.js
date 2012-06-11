009b8bc1f1817ff3146ef70643744f93210ce377
Etan Zapinsky <etan.zapinsky@gmail.com>
2012-06-10 21:46:23 -0400

    changed datepicker

--- popup.js
+++ popup.js
@@ -110,6 +110,14 @@ jQuery(function($) {
             className = 'div.' + $(value).attr('class');
             $(className).hide();
         });
+        
+        $('#date').DatePicker({
+        	flat: true,
+        	date: '2008-07-31',
+        	current: '2008-07-31',
+        	calendars: 1,
+        	starts: 1
+        });
     });
 });
 
